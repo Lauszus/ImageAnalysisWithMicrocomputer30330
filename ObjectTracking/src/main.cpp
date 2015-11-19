5b0e159c0c7b40c9d9c3733e7321339eac149523
Kristian Sloth Lauszus <lauszus@gmail.com>
2015-11-19 21:06:37 +0100

    Use while loop instead of goto statement

--- ObjectTracking/src/main.cpp
+++ ObjectTracking/src/main.cpp
@@ -127,199 +127,198 @@ int main(int argc, char *argv[]) {
     capture.set(CV_CAP_PROP_FRAME_WIDTH, 320);
     capture.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
 
-restart:
-    if (DEBUG && valueChanged) {
-        valueChanged = false;
-        printf("HSV: %u %u\t%u %u\t%u %u\t\tSize: %d %d\tFractile filter: %d %d\tNeighbor size: %d\tObject: %d %d\tPadding: %d\n", iLowH, iHighH, iLowS, iHighS, iLowV, iHighV, size1, size2, windowSize, percentile, neighborSize, objectMin, objectMax, cropPadding);
-    }
-
-    Mat image;
-    if (!capture.read(image)) {
-        printf("Could not read Webcam\n");
-        return 1;
-    }
-    flip(image, image, 1); // Flip image so it acts like a mirror
-
-#if 0
-    printf("image size: %lu, width: %d, height: %d, %lu, color format: %d\n",
-            image.total() * image.channels(), image.size().width, image.size().height, image.total(), image.channels());
-#endif
-    //imshow("Image", image);
-
-    Mat image_hsv;
-    cvtColor(image, image_hsv, COLOR_BGR2HSV); // Convert image to HSV
-
-    Mat imgThresholded(image.size(), CV_8UC1);
-    Scalar low = Scalar(iLowH, iLowS, iLowV);
-    Scalar high = Scalar(iHighH, iHighS, iHighV);
-
-    // Threshold the image
-#if 1
-    size_t index = 0;
-    for (size_t i = 0; i < image_hsv.total(); i++) {
-        bool inRange = true;
-        for (uint8_t j = 0; j < image_hsv.channels(); j++) {
-            uchar value = image_hsv.data[index + j];
-            if (j != 0 || low.val[j] < high.val[j]) {
-                if (value < low.val[j] || value > high.val[j])
-                    inRange = false;
-            } else if (value < low.val[j] && value > high.val[j]) // Needed for red color where H value will wrap around [170;10]
-                inRange = false;
+    while (cvWaitKey(1) != 27) { // Wait 1ms to see if ESC is pressed
+        if (DEBUG && valueChanged) {
+            valueChanged = false;
+            printf("HSV: %u %u\t%u %u\t%u %u\t\tSize: %d %d\tFractile filter: %d %d\tNeighbor size: %d\tObject: %d %d\tPadding: %d\n", iLowH, iHighH, iLowS, iHighS, iLowV, iHighV, size1, size2, windowSize, percentile, neighborSize, objectMin, objectMax, cropPadding);
         }
-        imgThresholded.data[i] = inRange ? 255 : 0;
-        index += image_hsv.channels();
-    }
-#else
-    inRange(image_hsv, low, high, imgThresholded);
-#endif
-    //imshow("Thresholded image", imgThresholded);
 
-    // Apply fractile filter to remove salt- and pepper noise
-    //double timer = (double)getTickCount();
-    Mat fractileFilterImg = fractileFilter(&imgThresholded, windowSize, percentile, true);
-    //printf("ms = %f\n", ((double)getTickCount() - timer) / getTickFrequency() * 1000.0);
-    //imshow("Fractile filter", fractileFilterImg);
+        Mat image;
+        if (!capture.read(image)) {
+            printf("Could not read Webcam\n");
+            return 1;
+        }
+        flip(image, image, 1); // Flip image so it acts like a mirror
 
-    // Crop image, so we are only looking at the actual data
-    index = 0;
-    int minX, maxX, minY, maxY;
-    minX = fractileFilterImg.size().width - 1;
-    minY = fractileFilterImg.size().height - 1;
-    maxX = maxY = 0;
-    for (size_t y = 0; y < fractileFilterImg.size().height; y++) {
-            for (size_t x = 0; x < fractileFilterImg.size().width; x++) {
-                if (fractileFilterImg.data[index]) { // Look for all white pixels
-                    if (x < minX)
-                        minX = x;
-                    else if (x > maxX)
-                        maxX = x;
-                    if (y < minY)
-                        minY = y;
-                    else if (y > maxY)
-                        maxY = y;
-                }
-                index += fractileFilterImg.channels();
+    #if 0
+        printf("image size: %lu, width: %d, height: %d, %lu, color format: %d\n",
+                image.total() * image.channels(), image.size().width, image.size().height, image.total(), image.channels());
+    #endif
+        //imshow("Image", image);
+
+        Mat image_hsv;
+        cvtColor(image, image_hsv, COLOR_BGR2HSV); // Convert image to HSV
+
+        Mat imgThresholded(image.size(), CV_8UC1);
+        Scalar low = Scalar(iLowH, iLowS, iLowV);
+        Scalar high = Scalar(iHighH, iHighS, iHighV);
+
+        // Threshold the image
+    #if 1
+        size_t index = 0;
+        for (size_t i = 0; i < image_hsv.total(); i++) {
+            bool inRange = true;
+            for (uint8_t j = 0; j < image_hsv.channels(); j++) {
+                uchar value = image_hsv.data[index + j];
+                if (j != 0 || low.val[j] < high.val[j]) {
+                    if (value < low.val[j] || value > high.val[j])
+                        inRange = false;
+                } else if (value < low.val[j] && value > high.val[j]) // Needed for red color where H value will wrap around [170;10]
+                    inRange = false;
             }
-    }
-    //printf("%d,%d,%d,%d\n", minX, maxX, minY, maxY);
+            imgThresholded.data[i] = inRange ? 255 : 0;
+            index += image_hsv.channels();
+        }
+    #else
+        inRange(image_hsv, low, high, imgThresholded);
+    #endif
+        //imshow("Thresholded image", imgThresholded);
 
-    if (maxX == 0 || maxY == 0)
-        goto restart;
+        // Apply fractile filter to remove salt- and pepper noise
+        //double timer = (double)getTickCount();
+        Mat fractileFilterImg = fractileFilter(&imgThresholded, windowSize, percentile, true);
+        //printf("ms = %f\n", ((double)getTickCount() - timer) / getTickFrequency() * 1000.0);
+        //imshow("Fractile filter", fractileFilterImg);
 
-    minX -= cropPadding;
-    minY -= cropPadding;
-    if (minX < 0)
-        minX = 0;
-    if (minY < 0)
-        minY = 0;
+        // Crop image, so we are only looking at the actual data
+        index = 0;
+        int minX, maxX, minY, maxY;
+        minX = fractileFilterImg.size().width - 1;
+        minY = fractileFilterImg.size().height - 1;
+        maxX = maxY = 0;
+        for (size_t y = 0; y < fractileFilterImg.size().height; y++) {
+                for (size_t x = 0; x < fractileFilterImg.size().width; x++) {
+                    if (fractileFilterImg.data[index]) { // Look for all white pixels
+                        if (x < minX)
+                            minX = x;
+                        else if (x > maxX)
+                            maxX = x;
+                        if (y < minY)
+                            minY = y;
+                        else if (y > maxY)
+                            maxY = y;
+                    }
+                    index += fractileFilterImg.channels();
+                }
+        }
+        //printf("%d,%d,%d,%d\n", minX, maxX, minY, maxY);
 
-    int width = maxX - minX;
-    int height = maxY - minY;
-    width += cropPadding;
-    height += cropPadding;
-    if (minX + width >= fractileFilterImg.size().width)
-        width = fractileFilterImg.size().width - 1 - minX;
-    if (minY + height >= fractileFilterImg.size().height)
-        height = fractileFilterImg.size().height - 1 - minY;
+        if (maxX == 0 || maxY == 0)
+            continue;
 
-    fractileFilterImg = Mat(fractileFilterImg, Rect(minX, minY, width, height)).clone();
+        minX -= cropPadding;
+        minY -= cropPadding;
+        if (minX < 0)
+            minX = 0;
+        if (minY < 0)
+            minY = 0;
 
-    // Apply morphological closing and opening
-    Mat morphologicalFilter = fractileFilterImg.clone();
+        int width = maxX - minX;
+        int height = maxY - minY;
+        width += cropPadding;
+        height += cropPadding;
+        if (minX + width >= fractileFilterImg.size().width)
+            width = fractileFilterImg.size().width - 1 - minX;
+        if (minY + height >= fractileFilterImg.size().height)
+            height = fractileFilterImg.size().height - 1 - minY;
 
-    // Morphological closing (fill small holes in the foreground)
-    dilate(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size2, size2)));
-    erode(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size2, size2)));
+        fractileFilterImg = Mat(fractileFilterImg, Rect(minX, minY, width, height)).clone();
 
-    // Morphological opening (remove small objects from the foreground)
-    erode(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size1, size1)));
-    dilate(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size1, size1)));
+        // Apply morphological closing and opening
+        Mat morphologicalFilter = fractileFilterImg.clone();
 
-    //imshow("Morphological", morphologicalFilter);
+        // Morphological closing (fill small holes in the foreground)
+        dilate(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size2, size2)));
+        erode(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size2, size2)));
 
-    // Create a image for each segment
-    uint8_t nSegments;
-    Mat *segments = getSegments(&morphologicalFilter, &nSegments, neighborSize, true);
+        // Morphological opening (remove small objects from the foreground)
+        erode(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size1, size1)));
+        dilate(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size1, size1)));
 
-    // Draw red contour if object is found
-    for (uint8_t i = 0; i < nSegments; i++) {
-        moments_t moments = calculateMoments(&segments[i], true);
-        int16_t eulerNumber = calculateEulerNumber(&segments[i], CONNECTED_8, true);
+        //imshow("Morphological", morphologicalFilter);
 
-        // Object detected if it is within the range of the invariant and Euler number is equal to 1
-        if (moments.phi1 > (float)objectMin * 1e-4f && moments.phi1 < (float)objectMax * 1e-4f && eulerNumber == 1) {
-            const float sideLength = sqrtf(moments.area); // Calculate side length from area
-            const float hypotenuse = sqrtf(2 * sideLength * sideLength); // Calculate hypotenuse, assuming that it is square
-            //printf("Area: %f %f %f\n", moments.area, sideLength, hypotenuse);
-            moments.centerX += minX; // Convert to x,y coordinates in original image
-            moments.centerY += minY;
-            image = drawMoments(&image, &moments, hypotenuse / 9.0f, 0); // Draw center of mass on original image
-#if 0 // Use Laplacian filter with lowpass filter to draw the contour
-            static LinearFilter lowpassLaplacianFilter = LaplacianFilter() + 9 * LowpassFilter();
-            Mat contour = lowpassLaplacianFilter.apply(&segments[i]); // Calculate contour
-#elif 0 // Use Laplacian filter to draw the contour
-            static LaplacianFilter laplacianFilter;
-            Mat contour = laplacianFilter.apply(&segments[i]); // Calculate contour
-#else // Use contour search method
-            Mat contour;
-            if (contoursSearch(&segments[i], &contour, CONNECTED_8, true)) // As there is only one segment it is faster to use the manual approach
-#endif
-            {
-                //imshow("LaplacianFilter", contour);
+        // Create a image for each segment
+        uint8_t nSegments;
+        Mat *segments = getSegments(&morphologicalFilter, &nSegments, neighborSize, true);
 
-                index = 0;
-                for (size_t y = 0; y < contour.size().height; y++) {
-                    for (size_t x = 0; x < contour.size().width; x++) {
-                        if (contour.data[index]) {
-                            size_t subIndex = ((x + minX) + (y + minY) * image.size().width) * image.channels(); // Convert to x,y coordinates in original image
-                            image.data[subIndex + 0] = 0;
-                            image.data[subIndex + 1] = 0;
-                            image.data[subIndex + 2] = 255; // Draw red contour
+        // Draw red contour if object is found
+        for (uint8_t i = 0; i < nSegments; i++) {
+            moments_t moments = calculateMoments(&segments[i], true);
+            int16_t eulerNumber = calculateEulerNumber(&segments[i], CONNECTED_8, true);
+
+            // Object detected if it is within the range of the invariant and Euler number is equal to 1
+            if (moments.phi1 > (float)objectMin * 1e-4f && moments.phi1 < (float)objectMax * 1e-4f && eulerNumber == 1) {
+                const float sideLength = sqrtf(moments.area); // Calculate side length from area
+                const float hypotenuse = sqrtf(2 * sideLength * sideLength); // Calculate hypotenuse, assuming that it is square
+                //printf("Area: %f %f %f\n", moments.area, sideLength, hypotenuse);
+                moments.centerX += minX; // Convert to x,y coordinates in original image
+                moments.centerY += minY;
+                image = drawMoments(&image, &moments, hypotenuse / 9.0f, 0); // Draw center of mass on original image
+    #if 0 // Use Laplacian filter with lowpass filter to draw the contour
+                static LinearFilter lowpassLaplacianFilter = LaplacianFilter() + 9 * LowpassFilter();
+                Mat contour = lowpassLaplacianFilter.apply(&segments[i]); // Calculate contour
+    #elif 0 // Use Laplacian filter to draw the contour
+                static LaplacianFilter laplacianFilter;
+                Mat contour = laplacianFilter.apply(&segments[i]); // Calculate contour
+    #else // Use contour search method
+                Mat contour;
+                if (contoursSearch(&segments[i], &contour, CONNECTED_8, true)) // As there is only one segment it is faster to use the manual approach
+    #endif
+                {
+                    //imshow("LaplacianFilter", contour);
+
+                    index = 0;
+                    for (size_t y = 0; y < contour.size().height; y++) {
+                        for (size_t x = 0; x < contour.size().width; x++) {
+                            if (contour.data[index]) {
+                                size_t subIndex = ((x + minX) + (y + minY) * image.size().width) * image.channels(); // Convert to x,y coordinates in original image
+                                image.data[subIndex + 0] = 0;
+                                image.data[subIndex + 1] = 0;
+                                image.data[subIndex + 2] = 255; // Draw red contour
+                            }
+                            index++;
                         }
-                        index++;
                     }
                 }
-            }
-        } /*else if (DEBUG)
-            printf("Segment: %u\tPhi: %.4f,%.4f\tEuler number: %d\tSegments: %u\n", i, moments.phi1, moments.phi2, eulerNumber, nSegments);*/
+            } /*else if (DEBUG)
+                printf("Segment: %u\tPhi: %.4f,%.4f\tEuler number: %d\tSegments: %u\n", i, moments.phi1, moments.phi2, eulerNumber, nSegments);*/
+        }
+
+        if (DEBUG) {
+            // Create windows
+            Mat window1(2 * image.size().height, image.size().width, CV_8UC3);
+            Mat top1(window1, Rect(0, 0, image.size().width, image.size().height));
+            Mat bot1(window1, Rect(0, image.size().height, imgThresholded.size().width, imgThresholded.size().height));
+
+            Mat window2(2 * fractileFilterImg.size().height, fractileFilterImg.size().width, CV_8UC3);
+            Mat top2(window2, Rect(0, 0, fractileFilterImg.size().width, fractileFilterImg.size().height));
+            Mat bot2(window2, Rect(0, fractileFilterImg.size().height, morphologicalFilter.size().width, morphologicalFilter.size().height));
+
+            // Convert to color images, so it actually shows up
+            cvtColor(imgThresholded, imgThresholded, COLOR_GRAY2BGR);
+            cvtColor(fractileFilterImg, fractileFilterImg, COLOR_GRAY2BGR);
+            cvtColor(morphologicalFilter, morphologicalFilter, COLOR_GRAY2BGR);
+
+            // Copy images to windows
+            image.copyTo(top1);
+            imgThresholded.copyTo(bot1);
+            fractileFilterImg.copyTo(top2);
+            morphologicalFilter.copyTo(bot2);
+
+            imshow("Window1", window1);
+            imshow("Window2", window2);
+        } else {
+            resize(image, image, image.size() * 2); // Resize image
+            imshow("Window", image);
+        }
+
+    #if 0
+        histogram_t histogram = getHistogram(&image);
+        Mat hist = drawHistogram(&histogram, &image, image.size());
+        imshow("Histogram", hist);
+    #endif
+
     }
-
-    if (DEBUG) {
-        // Create windows
-        Mat window1(2 * image.size().height, image.size().width, CV_8UC3);
-        Mat top1(window1, Rect(0, 0, image.size().width, image.size().height));
-        Mat bot1(window1, Rect(0, image.size().height, imgThresholded.size().width, imgThresholded.size().height));
-
-        Mat window2(2 * fractileFilterImg.size().height, fractileFilterImg.size().width, CV_8UC3);
-        Mat top2(window2, Rect(0, 0, fractileFilterImg.size().width, fractileFilterImg.size().height));
-        Mat bot2(window2, Rect(0, fractileFilterImg.size().height, morphologicalFilter.size().width, morphologicalFilter.size().height));
-
-        // Convert to color images, so it actually shows up
-        cvtColor(imgThresholded, imgThresholded, COLOR_GRAY2BGR);
-        cvtColor(fractileFilterImg, fractileFilterImg, COLOR_GRAY2BGR);
-        cvtColor(morphologicalFilter, morphologicalFilter, COLOR_GRAY2BGR);
-
-        // Copy images to windows
-        image.copyTo(top1);
-        imgThresholded.copyTo(bot1);
-        fractileFilterImg.copyTo(top2);
-        morphologicalFilter.copyTo(bot2);
-
-        imshow("Window1", window1);
-        imshow("Window2", window2);
-    } else {
-        resize(image, image, image.size() * 2); // Resize image
-        imshow("Window", image);
-    }
-
-#if 0
-    histogram_t histogram = getHistogram(&image);
-    Mat hist = drawHistogram(&histogram, &image, image.size());
-    imshow("Histogram", hist);
-#endif
-
-    if (cvWaitKey(1) != 27) // Wait 1ms to see if ESC is pressed
-        goto restart;
 
     return 0;
 }
