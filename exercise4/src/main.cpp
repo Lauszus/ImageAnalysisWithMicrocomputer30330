9a0d8203f50c99a3a8bb2232959ee25626902700
Kristian Sloth Lauszus <lauszus@gmail.com>
2015-12-20 03:40:16 +0100

    Use own morphological filter in exercise 4

--- exercise4/src/main.cpp
+++ exercise4/src/main.cpp
@@ -18,6 +18,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
+#include "filter.h"
 #include "histogram.h"
 #include "contours.h"
 #include "misc.h"
@@ -36,12 +37,12 @@ int main(int argc, char *argv[]) {
     cvNamedWindow(controlWindow, CV_WINDOW_AUTOSIZE); // Create a window called "Control"
 
     int thresholdValue = 105;
-    int size1 = 5;
-    int size2 = 5;
+    int closingSize = 3;
+    int openingSize = 3;
 
     cvCreateTrackbar("Threshold", controlWindow, &thresholdValue, 255, valueChangedCallBack);
-    cvCreateTrackbar("Size1", controlWindow, &size1, 10, valueChangedCallBack);
-    cvCreateTrackbar("Size2", controlWindow, &size2, 10, valueChangedCallBack);
+    cvCreateTrackbar("Closing size", controlWindow, &closingSize, 10, valueChangedCallBack);
+    cvCreateTrackbar("Opening size", controlWindow, &openingSize, 10, valueChangedCallBack);
 
     moveWindow(controlWindow, 0, 500); // Move window
 
@@ -57,7 +58,7 @@ int main(int argc, char *argv[]) {
     imwrite("img/image.png", image);
 
 restart:
-    printf("Threshold: %d\tSize: %d,%d\n", thresholdValue, size1, size2);
+    printf("Threshold: %d\tSize: %d,%d\n", thresholdValue, closingSize, openingSize);
 
     histogram_t histogram = getHistogram(&image);
     Mat hist = drawHistogram(&histogram, &image, Size(image.size().width * 2, image.size().height), thresholdValue);
@@ -72,23 +73,23 @@ restart:
     imwrite("img/imageThreshold.png", imageThresholdBorder);
 
     // Apply morphological closing and opening
-    Mat morphologicalFilter = imageThreshold.clone();
+    Mat morphologicalFilterImg = imageThreshold.clone();
 
-    // Morphological closing (fill small holes in the foreground)
-    dilate(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size2, size2)));
-    erode(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size2, size2)));
+    // Morphological closing (Remove small bright spots (i.e. "salt") and connect small dark cracks)
+    morphologicalFilterImg = morphologicalFilter(&morphologicalFilterImg, DILATION, closingSize, false);
+    morphologicalFilterImg = morphologicalFilter(&morphologicalFilterImg, EROSION, closingSize, false);
 
-    // Morphological opening (remove small objects from the foreground)
-    erode(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size1, size1)));
-    dilate(morphologicalFilter, morphologicalFilter, getStructuringElement(MORPH_ELLIPSE, Size(size1, size1)));
+    // Morphological opening (Remove small dark spots (i.e. "pepper") and connect small bright cracks)
+    morphologicalFilterImg = morphologicalFilter(&morphologicalFilterImg, EROSION, openingSize, false);
+    morphologicalFilterImg = morphologicalFilter(&morphologicalFilterImg, DILATION, openingSize, false);
 
-    imshow("Morphological filter", morphologicalFilter);
-    Mat morphologicalFilterBorder;
-    copyMakeBorder(morphologicalFilter, morphologicalFilterBorder, 1, 1, 1, 1, BORDER_CONSTANT); // Add a border before writing
-    imwrite("img/morphologicalFilter.png", morphologicalFilterBorder);
+    imshow("Morphological filter", morphologicalFilterImg);
+    Mat morphologicalFilterImgBorder;
+    copyMakeBorder(morphologicalFilterImg, morphologicalFilterImgBorder, 1, 1, 1, 1, BORDER_CONSTANT); // Add a border before writing
+    imwrite("img/morphologicalFilter.png", morphologicalFilterImgBorder);
 
     Mat contour;
-    if (contoursSearch(&morphologicalFilter, &contour, CONNECTED_8, false)) {
+    if (contoursSearch(&morphologicalFilterImg, &contour, CONNECTED_8, false)) {
         imshow("Contour", contour);
         imwrite("img/contour.png", contour);
     }
